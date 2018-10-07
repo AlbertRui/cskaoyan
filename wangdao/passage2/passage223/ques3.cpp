d9c8cdea9d100ba9f90d4988d69da10987b58bf7
AlbertRui <isruizhang@gmail.com>
2018-10-07 23:21:12 +0800

    modified

--- wangdao/passage2/passage223/ques3.cpp
+++ wangdao/passage2/passage223/ques3.cpp
@@ -2,10 +2,19 @@
 // Created by Administrator on 2018/10/7.
 //
 #include "common.h"
-void del_x_l(SqList &L, ElemType x) {
+void del_x_1(SqList &L, ElemType x) {
     int k = 0;
     for (int i = 0; i < L.length; ++i)
         if(L.data[i] != x) L.data[k++] = L.data[i];
     L.length = k;
+}
+void del_x_2(SqList &L, ElemType x) {
+    int k = 0, i;
+    while (i < L.length) {
+        if (L.data[i] == k) k++;
+        else L.data[i - k] = L.data[i];
+        i ++;
+    }
+    L.length = L.length - k;
 }
 
