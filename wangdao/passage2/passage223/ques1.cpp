abfd79b8e93b152a769f25191c6415a17db0912c
AlbertRui <isruizhang@gmail.com>
2018-10-07 22:54:51 +0800

    update question 1

--- wangdao/passage2/passage223/ques1.cpp
+++ wangdao/passage2/passage223/ques1.cpp
@@ -7,4 +7,18 @@ typedef struct {
     ElemType data[MaxSize];
     int length;
 }SqList;
+bool Del_Min(SqList &L, ElemType &value) {
+    if (L.length == 0) return false;
+    value = L.data[0];
+    int pos = 0;
+    for (int i = 1; i < L.length; i++) {
+        if(L.data[i] < value) {
+            value = L.data[i];
+            pos = i;
+        }
+    }
+    L.data[pos] = L.data[L.length - 1];
+    L.length --;
+    return true;
+}
 
