467e36a5b6ebbdb3e3f0f9dc059568826952a0fc
AlbertRui <isruizhang@gmail.com>
2018-11-10 23:32:34 +0800

    add my solutions

---
+++ wangdao/passage2/passage223/ques8.cpp
@@ -0,0 +1,15 @@
+typedef int DataType;
+void Reverse(DataType A[], int left, int right, int arraySize) {
+    if(left >= right || right >= arraySize) return;
+    int mid = (right + left) / 2;
+    for(int i = 0; i <= mid - left; i++) {
+        DataType temp = A[left + i];
+        A[left + i] = A[right - i];
+        A[right - i] = temp; 
+    }
+}
+void Exchange(DataType A, int m, int n, int arraySize) {
+    Reverse(A, 0, m + n - 1, arraySize);
+    Reverse(A, 0, n - 1, arraySize);
+    Reverse(A, n, m + n - 1, arraySize);
+}
