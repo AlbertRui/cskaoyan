d221af400260b83ae9c6c8632f84b9e9b3779cef
AlbertRui <isruizhang@gmail.com>
2020-03-10 23:53:10 +0800

    update solutions

--- wangdao/passage2/passage223/ques6.cpp
+++
@@ -1,9 +0,0 @@
-#include "common.h"
-bool Delete_Same(SqList& L) {
-    if(L.length == 0) return false;
-    int i, j;
-    for (int i = 0, j = 1; j < L.length; ++i)
-        if(L.data[i] != L.data[j]) L.data[++i] = L.data[j];
-    L.length = i + 1;
-    return true;
-}
