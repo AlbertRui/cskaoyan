c3c4cc2c6978f957323b2fe17906322344aa62cf
AlbertRui <isruizhang@gmail.com>
2020-03-15 15:56:52 +0800

    单链表数据结构

---
+++ chapter2/section3/entity/ListNode.h
@@ -0,0 +1,10 @@
+/*
+ * @Author       : Ryan Zhang
+ * @Date         : 2020-03-15 15:34:52
+ * @LastEditTime : 2020-03-15 15:38:34
+ * @Descripttion : 单链表的定义
+ */
+struct ListNode {
+    ListNode *next;
+    int data;
+};
