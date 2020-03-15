67e9e9bcf83b9271f4c2a8f75021f9f4451d036a
AlbertRui <isruizhang@gmail.com>
2020-03-15 18:28:05 +0800

    add constructor for ListNode.h

--- chapter2/section3/entity/ListNode.h
+++ chapter2/section3/entity/ListNode.h
@@ -1,10 +1,16 @@
 /*
  * @Author       : Ryan Zhang
  * @Date         : 2020-03-15 15:34:52
- * @LastEditTime : 2020-03-15 15:38:34
+ * @LastEditTime : 2020-03-15 16:41:03
  * @Descripttion : 单链表的定义
  */
+#ifndef LIST_NODE_H
+#define LIST_NODE_H
+
 struct ListNode {
     ListNode *next;
-    int data;
+    int val;
+    ListNode(int val) : val(val), next(NULL) {}
 };
+
+#endif
