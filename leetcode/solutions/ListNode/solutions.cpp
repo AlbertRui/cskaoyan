9afea4f730f9e21702252554e2909d2bd3fabfdb
AlbertRui <isruizhang@gmail.com>
2021-01-15 16:00:07 +0800

    add my solutions

--- leetcode/solutions/ListNode/solutions.cpp
+++ leetcode/solutions/ListNode/solutions.cpp
@@ -1,11 +1,16 @@
 /*
  * @Author       : Ryan Zhang
  * @Date         : 2021-01-15 14:18:46
- * @LastEditTime : 2021-01-15 14:55:41
+ * @LastEditTime : 2021-01-15 15:59:34
  * @Descripttion : ListNode cases for LeetCode
  */
 
+#include <vector>
+#include <queue>
+
 #include "../../entity/ListNode.h"
+
+using namespace std;
 
 class solutoins {
     /**
@@ -39,5 +44,58 @@ class solutoins {
         }
         slow->next = slow->next->next;
         return res->next;
+    }
+
+    /**
+     * 21.合并两个升序链表
+     */
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode *dummy = new ListNode(0), *p = dummy;
+        while (l1 and l2) {
+            if (l1->val < l2->val) {
+                p->next = l1;
+                l1 = l1->next;
+            } else {
+                p->next = l2;
+                l2 = l2->next;
+            } 
+            p = p->next;
+        }
+        if (l2) {
+            l1 = l2;
+        }
+        while (l1) {
+            p->next = l1;
+            l1 = l1->next;
+            p = p->next;
+        }
+        return dummy->next;
+    }
+
+    /**
+     * 23.合并k个升序链表
+     */
+    struct compare {
+        bool operator() (const ListNode* l, const ListNode* r) {
+            return l->val > r->val;
+        }
+    };
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        priority_queue<ListNode*, vector<ListNode*>, compare> pq;
+        for (ListNode* list : lists) {
+            if (list) {
+                pq.push(list);
+            }
+        }
+        ListNode* dummy = new ListNode(0), *tail = dummy;
+        while (!pq.empty()) {
+            tail->next = pq.top();
+            pq.pop();
+            tail = tail->next;
+            if (tail->next) {
+                pq.push(tail->next);
+            }
+        }
+        return dummy->next;
     }
 };
